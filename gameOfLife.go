91ad3d02aa20adca048e3930070295c70c20a6cd
Tyler Coare <tyler.coare@gmail.com>
2019-07-11 16:53:40 +0100

    Added comments

--- gameOfLife.go
+++ gameOfLife.go
@@ -7,10 +7,9 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 
-	// deadState := deadState(7, 6)
-	// render(deadState)
+	// Generates random seed so that results are different on each run
+	rand.Seed(time.Now().UnixNano())
 
 	randomState := randomState(7, 6)
 	render(randomState)
@@ -19,8 +18,10 @@ func main() {
 
 func deadState(width, height int) [][]int {
 
+	// Creates board which is a slice containing height number of slices
 	board := make([][]int, height)
 
+	// loop to append 0 in width elements in each height slice in board
 	for i := 0; i < height; i++ {
 		for x := 0; x < width; x++ {
 			board[i] = append(board[i], 0)
@@ -41,8 +42,10 @@ func deadState(width, height int) [][]int {
 
 func randomState(width, height int) [][]int {
 
+	// Uses previous function to create a board which is zero entried
 	board := deadState(width, height)
 
+	// Loops through board and replaces each 0 value with a random number
 	for x := 0; x < height; x++ {
 		for y := 0; y < width; y++ {
 			board[x][y] = rand.Intn(2)
@@ -63,6 +66,8 @@ func randomState(width, height int) [][]int {
 
 func render(board [][]int) {
 
+	// Loops through each row and creates a new slice of strings. Each
+	// element is then checked to see if it is 'alive' or 'dead'
 	for _, row := range board {
 		renderRow := make([]string, len(row))
 		for index, state := range row {
